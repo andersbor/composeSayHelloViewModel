dad4d9c2d898c82df0297608eea960bd2ce576c8
andersbor <anbo@zealand.dk>
2024-09-16 20:17:35 +0200

    gradle update

--- app/src/main/java/com/example/sayhelloviewmodel/MainActivity.kt
+++ app/src/main/java/com/example/sayhelloviewmodel/MainActivity.kt
@@ -3,12 +3,7 @@ package com.example.sayhelloviewmodel
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -22,13 +17,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             SayHelloViewModelTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    //color = MaterialTheme.colorScheme.background
-                    color = Color.Yellow
-                ) {
-                    MainScreen()
-                }
+                MainScreen()
             }
         }
     }
